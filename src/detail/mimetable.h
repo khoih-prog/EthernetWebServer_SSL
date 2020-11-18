5ca14d3b32dffc8e8f6caa4bd7e5c28161b200b9
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2020-11-18 02:48:48 -0500

    v1.1.1
    ### Release v1.1.1
    
    1. Permit sites with "Chain could not be linked to a trust anchor" such as mqtt.thingstream.io.
    2. Add example MQTTS_ThingStream to demonstrate new feature

--- src/detail/mimetable.h
+++ src/detail/mimetable.h
@@ -9,12 +9,13 @@
   Built by Khoi Hoang https://github.com/khoih-prog/EthernetWebServer_SSL
   Licensed under MIT license
        
-  Version: 1.1.0
+  Version: 1.1.1
 
   Version Modified By   Date      Comments
   ------- -----------  ---------- -----------
   1.1.0   K Hoang      14/11/2020 Initial coding for SAMD21/SAMD51, nRF52, SAM DUE to support Ethernet shields using SSL.
                                   Supporting W5x00 using Ethernetx, ENC28J60 using EthernetENC and UIPEthernet libraries
+  1.1.1   K Hoang      18/11/2020 Permit sites with "Chain could not be linked to a trust anchor" such as ThingStream  
  *****************************************************************************************************************************/
  
 #pragma once
