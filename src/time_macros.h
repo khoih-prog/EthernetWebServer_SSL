d1a78f93967f36fda904295ffe668504a56d3dde
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2020-11-19 16:10:17 -0500

    v1.1.2
    ### Release v1.1.2
    
    1. Add SSL debug feature.
    2. Enhance examples.

--- src/time_macros.h
+++ src/time_macros.h
@@ -8,13 +8,14 @@
   Built by Khoi Hoang https://github.com/khoih-prog/EthernetWebServer_SSL
   Licensed under MIT license
        
-  Version: 1.1.1
+  Version: 1.1.2
 
   Version Modified By   Date      Comments
   ------- -----------  ---------- -----------
   1.1.0   K Hoang      14/11/2020 Initial coding for SAMD21/SAMD51, nRF52, SAM DUE to support Ethernet shields using SSL.
                                   Supporting W5x00 using Ethernetx, ENC28J60 using EthernetENC and UIPEthernet libraries
-  1.1.1   K Hoang      18/11/2020 Permit sites with "Chain could not be linked to a trust anchor" such as ThingStream  
+  1.1.1   K Hoang      18/11/2020 Permit sites with "Chain could not be linked to a trust anchor" such as ThingStream
+  1.1.2   K Hoang      19/11/2020 Add SSL debug feature. Enhance examples.
  *****************************************************************************************************************************/
 
 #include "SSLClient/time_macros.h"
