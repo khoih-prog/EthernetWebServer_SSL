13057e1e71ffa7d708934f3895b37b8d8e0df26a
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-03-02 21:42:22 -0500

    v1.7.5 to fix decoding error bug
    ### Releases v1.7.5
    
    1. Fix decoding error bug when using special `&` in data fields. Check [Decoding Error. two times called urlDecode in Parsing-impl.h. #17](https://github.com/khoih-prog/WiFiWebServer/issues/17)
    2. Update `Packages' Patches`

--- src/libb64/cdecode.c
+++ src/libb64/cdecode.c
@@ -8,26 +8,19 @@
   Built by Khoi Hoang https://github.com/khoih-prog/EthernetWebServer_SSL
   Licensed under MIT license
        
-  Version: 1.7.4
+  Version: 1.7.5
 
   Version Modified By   Date      Comments
   ------- -----------  ---------- -----------
   1.1.0   K Hoang      14/11/2020 Initial coding for SAMD21/SAMD51, nRF52, SAM DUE to support Ethernet shields using SSL.
                                   Supporting W5x00 using Ethernetx, ENC28J60 using EthernetENC and UIPEthernet libraries
-  1.1.1   K Hoang      18/11/2020 Permit sites with "Chain could not be linked to a trust anchor" such as ThingStream
-  1.1.2   K Hoang      19/11/2020 Add SSL debug feature. Enhance examples.
-  1.2.0   K Hoang      20/11/2020 Add basic HTTP and WebSockets Client by merging ArduinoHttpClient
-  1.3.0   K Hoang      04/12/2020 Add support to NativeEthernet Library for Teensy 4.1
-  1.3.1   K Hoang      26/12/2020 Suppress all possible compiler warnings
-  1.4.0   K Hoang      13/05/2021 Add support to RP2040-based boards using Arduino mbed_rp2040 core
-  1.5.0   K Hoang      15/05/2021 Add support to RP2040-based boards using Arduino-pico rp2040 core
-  1.6.0   K Hoang      04/09/2021 Add support to QNEthernet Library for Teensy 4.1
-  1.6.1   K Hoang      04/10/2021 Change option for PIO `lib_compat_mode` from default `soft` to `strict`. Update Packages Patches
+  ...
   1.7.0   K Hoang      19/12/2021 Reduce usage of Arduino String with std::string. Add support to Portenta H7 Ethernet
   1.7.1   K Hoang      25/12/2021 Fix bug relating to String
   1.7.2   K Hoang      27/12/2021 Fix wrong http status header bug and authenticate issue caused by libb64
   1.7.3   K Hoang      11/01/2022 Fix libb64 compile error for ESP8266
   1.7.4   K Hoang      11/01/2022 Fix libb64 fallthrough compile warning
+  1.7.5   K Hoang      02/03/2022 Fix decoding error bug
  *****************************************************************************************************************************/
 
 #if !(ESP32 || ESP8266)
