e06411888c2c3eb585bba2c8522f589f93db4e68
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-08-25 23:40:17 -0400

    v1.9.1 to auto-select SPI(s) `SS/CS` pins
    ### Releases v1.9.1
    
    1. Auto-select SPI(s) `SS/CS` pins according to board package if available
    2. Update `Packages' Patches`

--- src/inner.h
+++ src/inner.h
@@ -7,7 +7,7 @@
   
   Built by Khoi Hoang https://github.com/khoih-prog/EthernetWebServer_SSL
        
-  Version: 1.9.0
+  Version: 1.9.1
 
   Version Modified By   Date      Comments
   ------- -----------  ---------- -----------
@@ -27,6 +27,7 @@
   1.8.1   K Hoang      09/04/2022 Add support to SPI1 for RP2040 using arduino-pico core
   1.8.2   K Hoang      27/04/2022 Change from `arduino.cc` to `arduino.tips` in examples
   1.9.0   K Hoang      05/05/2022 Add support to custom SPI for Teensy, Mbed RP2040, Portenta_H7, etc.
+  1.9.1   K Hoang      25/08/2022 Auto-select SPI SS/CS pin according to board package
  *****************************************************************************************************************************/
 
 #include "SSLClient/inner.h"
