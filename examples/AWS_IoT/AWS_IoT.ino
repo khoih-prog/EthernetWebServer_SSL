e866bf5c85b0ed803fbe47c95fe0201ec3a043d3
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-08-25 23:35:51 -0400

    v1.9.1 to auto-select SPI(s) `SS/CS` pins
    ### Releases v1.9.1
    
    1. Auto-select SPI(s) `SS/CS` pins according to board package if available
    2. Update `Packages' Patches`

--- examples/AWS_IoT/AWS_IoT.ino
+++ examples/AWS_IoT/AWS_IoT.ino
@@ -68,7 +68,7 @@ void callback(char* topic, byte* payload, unsigned int length)
   Serial.print(topic);
   Serial.print("] ");
   
-  for (int i = 0; i < length; i++)
+  for (unsigned int i = 0; i < length; i++)
   {
     Serial.print((char)payload[i]);
   }
--- examples/multiFileProject/multiFileProject.ino
+++ examples/multiFileProject/multiFileProject.ino
@@ -9,8 +9,8 @@
 
 // To demo how to include files in multi-file Projects
 
-#define ETHERNET_WEBSERVER_SSL_VERSION_MIN_TARGET      "EthernetWebServer_SSL v1.8.1"
-#define ETHERNET_WEBSERVER_SSL_VERSION_MIN             1008001
+#define ETHERNET_WEBSERVER_SSL_VERSION_MIN_TARGET      "EthernetWebServer_SSL v1.9.1"
+#define ETHERNET_WEBSERVER_SSL_VERSION_MIN             1009001
 
 #include "multiFileProject.h"
 
