7c8be7f4917cf5f7c29f4afa55207a054f0a6f63
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-09-07 00:54:11 -0400

    v1.9.2 to slow SPI clock only when necessary
    ### Releases v1.9.2
    
    1. Slow SPI clock for old `W5100` shield or `SAMD Zero`
    2. Use correct Debug Terminal `Serial` for so-called **SAMD21 Zero** boards from Arduino as well as Adafruit
    3. Update `Packages' Patches`

--- src/Ethernet_HTTPClient/Ethernet_URLEncoder.h
+++ src/Ethernet_HTTPClient/Ethernet_URLEncoder.h
@@ -8,27 +8,19 @@
   
   Built by Khoi Hoang https://github.com/khoih-prog/EthernetWebServer_SSL
        
-  Version: 1.9.1
+  Version: 1.9.2
 
   Version Modified By   Date      Comments
   ------- -----------  ---------- -----------
   1.1.0   K Hoang      14/11/2020 Initial coding for SAMD21/SAMD51, nRF52, SAM DUE to support Ethernet shields using SSL.
                                   Supporting W5x00 using Ethernetx, ENC28J60 using EthernetENC and UIPEthernet libraries
   ...
-  1.7.0   K Hoang      19/12/2021 Reduce usage of Arduino String with std::string. Add support to Portenta H7 Ethernet
-  1.7.1   K Hoang      25/12/2021 Fix bug relating to String
-  1.7.2   K Hoang      27/12/2021 Fix wrong http status header bug and authenticate issue caused by libb64
-  1.7.3   K Hoang      11/01/2022 Fix libb64 compile error for ESP8266
-  1.7.4   K Hoang      11/01/2022 Fix libb64 fallthrough compile warning
-  1.7.5   K Hoang      02/03/2022 Fix decoding error bug
-  1.7.6   K Hoang      14/03/2022 Fix bug when using QNEthernet staticIP. Add staticIP option to NativeEthernet
-  1.7.7   K Hoang      14/03/2022 Change licence from `MIT` to `GPLv3`
-  1.7.8   K Hoang      29/03/2022 Sync with `SSLClient` v1.6.11
   1.8.0   K Hoang      05/04/2022 Use Ethernet_Generic library as default. Support SPI2 for ESP32
   1.8.1   K Hoang      09/04/2022 Add support to SPI1 for RP2040 using arduino-pico core
   1.8.2   K Hoang      27/04/2022 Change from `arduino.cc` to `arduino.tips` in examples
   1.9.0   K Hoang      05/05/2022 Add support to custom SPI for Teensy, Mbed RP2040, Portenta_H7, etc.
   1.9.1   K Hoang      25/08/2022 Auto-select SPI SS/CS pin according to board package
+  1.9.2   K Hoang      07/09/2022 Slow SPI clock for old W5100 shield or SAMD Zero. Improve support for SAMD21
  *****************************************************************************************************************************/
  
 // Library to simplify HTTP fetching on Arduino
