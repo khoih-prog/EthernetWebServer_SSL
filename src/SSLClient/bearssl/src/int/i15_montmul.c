09a105f5d297133e0f94c84df3e1264718c5368e
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-10-26 23:09:23 -0400

    v1.9.3 to add support to Seeeduino nRF52
    ### Releases v1.9.3
    
    1. Add support to Seeeduino nRF52840-based boards such as **Seeed XIAO_NRF52840 and XIAO_NRF52840_SENSE**, etc. using Seeed `mbed` or `nRF52` core
    2. Add astyle using `allman` style. Restyle the library
    3. Display warning only when `_ETHERNET_WEBSERVER_LOGLEVEL_` > 3
    4. Update examples
    5. Update `Packages' Patches` to add Seeeduino `nRF52` core

--- src/SSLClient/bearssl/src/int/i15_montmul.c
+++ src/SSLClient/bearssl/src/int/i15_montmul.c
@@ -1,54 +1,58 @@
 /*
- * Copyright (c) 2017 Thomas Pornin <pornin@bolet.org>
- *
- * Permission is hereby granted, free of charge, to any person obtaining 
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be 
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+   Copyright (c) 2017 Thomas Pornin <pornin@bolet.org>
+
+   Permission is hereby granted, free of charge, to any person obtaining
+   a copy of this software and associated documentation files (the
+   "Software"), to deal in the Software without restriction, including
+   without limitation the rights to use, copy, modify, merge, publish,
+   distribute, sublicense, and/or sell copies of the Software, and to
+   permit persons to whom the Software is furnished to do so, subject to
+   the following conditions:
+
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
 
 #include "inner.h"
 
 /* see inner.h */
 void
 br_i15_montymul(uint16_t *d, const uint16_t *x, const uint16_t *y,
-	const uint16_t *m, uint16_t m0i)
+                const uint16_t *m, uint16_t m0i)
 {
-	size_t len, len4, u, v;
-	uint32_t dh;
+  size_t len, len4, u, v;
+  uint32_t dh;
 
-	len = (m[0] + 15) >> 4;
-	len4 = len & ~(size_t)3;
-	br_i15_zero(d, m[0]);
-	dh = 0;
-	for (u = 0; u < len; u ++) {
-		uint32_t f, xu, r, zh;
+  len = (m[0] + 15) >> 4;
+  len4 = len & ~(size_t)3;
+  br_i15_zero(d, m[0]);
+  dh = 0;
 
-		xu = x[u + 1];
-		f = MUL15((d[1] + MUL15(x[u + 1], y[1])) & 0x7FFF, m0i)
-			& 0x7FFF;
+  for (u = 0; u < len; u ++)
+  {
+    uint32_t f, xu, r, zh;
+
+    xu = x[u + 1];
+    f = MUL15((d[1] + MUL15(x[u + 1], y[1])) & 0x7FFF, m0i)
+        & 0x7FFF;
 #if BR_ARMEL_CORTEXM_GCC
-		if (len4 != 0) {
-			uint16_t *limit;
 
-			limit = d + len4;
-			asm volatile (
-"\n\
+    if (len4 != 0)
+    {
+      uint16_t *limit;
+
+      limit = d + len4;
+      asm volatile (
+        "\n\
 	@ carry: r=r2                                              \n\
 	@ multipliers: xu=r3 f=r4                                  \n\
 	@ base registers: d+v=r5 y+v=r6 m+v=r7                     \n\
@@ -126,59 +130,67 @@ loop%=:                                                            \n\
 		                                                   \n\
 	str	r2, %[carry]                                       \n\
 "
-: [carry] "=m" (r)
-: [xu] "m" (xu), [f] "m" (f), [d] "m" (d), [y] "m" (y),
-	[m] "m" (m), [limit] "m" (limit)
-: "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9" );
-		} else {
-			r = 0;
-		}
-		v = len4;
+        : [carry] "=m" (r)
+        : [xu] "m" (xu), [f] "m" (f), [d] "m" (d), [y] "m" (y),
+        [m] "m" (m), [limit] "m" (limit)
+        : "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9" );
+    }
+    else
+    {
+      r = 0;
+    }
+
+    v = len4;
 #else
-		r = 0;
-		for (v = 0; v < len4; v += 4) {
-			uint32_t z;
+    r = 0;
 
-			z = d[v + 1] + MUL15(xu, y[v + 1])
-				+ MUL15(f, m[v + 1]) + r;
-			r = z >> 15;
-			d[v + 0] = z & 0x7FFF;
-			z = d[v + 2] + MUL15(xu, y[v + 2])
-				+ MUL15(f, m[v + 2]) + r;
-			r = z >> 15;
-			d[v + 1] = z & 0x7FFF;
-			z = d[v + 3] + MUL15(xu, y[v + 3])
-				+ MUL15(f, m[v + 3]) + r;
-			r = z >> 15;
-			d[v + 2] = z & 0x7FFF;
-			z = d[v + 4] + MUL15(xu, y[v + 4])
-				+ MUL15(f, m[v + 4]) + r;
-			r = z >> 15;
-			d[v + 3] = z & 0x7FFF;
-		}
+    for (v = 0; v < len4; v += 4)
+    {
+      uint32_t z;
+
+      z = d[v + 1] + MUL15(xu, y[v + 1])
+          + MUL15(f, m[v + 1]) + r;
+      r = z >> 15;
+      d[v + 0] = z & 0x7FFF;
+      z = d[v + 2] + MUL15(xu, y[v + 2])
+          + MUL15(f, m[v + 2]) + r;
+      r = z >> 15;
+      d[v + 1] = z & 0x7FFF;
+      z = d[v + 3] + MUL15(xu, y[v + 3])
+          + MUL15(f, m[v + 3]) + r;
+      r = z >> 15;
+      d[v + 2] = z & 0x7FFF;
+      z = d[v + 4] + MUL15(xu, y[v + 4])
+          + MUL15(f, m[v + 4]) + r;
+      r = z >> 15;
+      d[v + 3] = z & 0x7FFF;
+    }
+
 #endif
-		for (; v < len; v ++) {
-			uint32_t z;
 
-			z = d[v + 1] + MUL15(xu, y[v + 1])
-				+ MUL15(f, m[v + 1]) + r;
-			r = z >> 15;
-			d[v + 0] = z & 0x7FFF;
-		}
+    for (; v < len; v ++)
+    {
+      uint32_t z;
 
-		zh = dh + r;
-		d[len] = zh & 0x7FFF;
-		dh = zh >> 15;
-	}
+      z = d[v + 1] + MUL15(xu, y[v + 1])
+          + MUL15(f, m[v + 1]) + r;
+      r = z >> 15;
+      d[v + 0] = z & 0x7FFF;
+    }
 
-	/*
-	 * Restore the bit length (it was overwritten in the loop above).
-	 */
-	d[0] = m[0];
+    zh = dh + r;
+    d[len] = zh & 0x7FFF;
+    dh = zh >> 15;
+  }
 
-	/*
-	 * d[] may be greater than m[], but it is still lower than twice
-	 * the modulus.
-	 */
-	br_i15_sub(d, m, NEQ(dh, 0) | NOT(br_i15_sub(d, m, 0)));
+  /*
+     Restore the bit length (it was overwritten in the loop above).
+  */
+  d[0] = m[0];
+
+  /*
+     d[] may be greater than m[], but it is still lower than twice
+     the modulus.
+  */
+  br_i15_sub(d, m, NEQ(dh, 0) | NOT(br_i15_sub(d, m, 0)));
 }
